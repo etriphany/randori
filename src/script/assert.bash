0f0551b6420264a10c93760738938fd5aa91e661
cadu.goncalves <cadu.goncalves@gmail.com>
2020-05-09 19:42:06 -0300

    Base setup

---
+++ src/script/assert.bash
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+# Extended from BashUnit:
+# https://github.com/djui/bashunit
+#
+
+# State
+asserts_passed=0
+asserts_failed=0
+asserts_skipped=0
+
+# Assert that a given expression evaluates to true.
+#
+# $1: Expression
+assert() {
+    if $* ; then _passed ; else _failed "$*" ; fi
+}
+
+# Assert that a given output string is equal to an expected string.
+#
+# $1: Output
+# $2: Expected
+assertEqualExp() {
+    echo $1 | grep -E "^$2$" > /dev/null
+    if [ $? -eq 0 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given output string is not equal to an expected string.
+#
+# $1: Output
+# $2: Expected
+assertNotEqualExp() {
+    echo $1 | grep -E "^$2$" > /dev/null
+    if [ $? -ne 0 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given output string starts with an expected string.
+#
+# $1: Output
+# $2: Expected
+assertStartsExp() {
+    echo $1 | grep -E "^$2" > /dev/null
+    if [ $? -eq 0 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given output string contains an expected string.
+#
+# $1: Output
+# $2: Expected
+assertContainsExp() {
+    echo $1 | grep -E "*$2*" > /dev/null
+    if [ $? -eq 0 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given output string is equal to an expected string.
+#
+# $1: Output
+# $2: Expected
+assertEqualStr() {
+    if [ $1 == $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given output string is not equal to an expected string.
+#
+# $1: Output
+# $2: Expected
+assertNotEqualStr() {
+    if [ $1 != $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that the last command's return code is equal to an expected integer.
+#
+# $1: Output
+# $2: Expected
+# $?: Provided
+assertReturn() {
+    local code=$?
+    if [ $code -eq $2 ] ; then _passed ; else _failed "$code" "$2" ; fi
+}
+
+# Assert that the last command's return code is not equal to an expected integer.
+#
+# $1: Output
+# $2: Expected
+# $?: Provided
+assertNotReturn() {
+    local code=$?
+    if [ $code -ne $2 ] ; then _passed ; else _failed "$code" "$2" ; fi
+}
+
+# Assert that a given integer is equal to an expected integer.
+#
+# $1: Output
+# $2: Expected
+assertEqual() {
+    if [ $1 -eq $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given integer is not equal to an expected integer.
+#
+# $1: Output
+# $2: Expected
+assertNotEqual() {
+    if [ $1 -ne $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given integer is greater than an expected integer.
+#
+# $1: Output
+# $2: Expected
+assertGreaterThan() {
+    if [ $1 -gt $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given integer is greater than or equal to an expected integer.
+#
+# $1: Output
+# $2: Expected
+assertAtLeast() {
+    if [ $1 -ge $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given integer is less than an expected integer.
+#
+# $1: Output
+# $2: Expected
+assertLessThan() {
+    if [ $1 -lt $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
+
+# Assert that a given integer is less than or equal to an expected integer.
+#
+# $1: Output
+# $2: Expected
+assertAtMost() {
+    if [ $1 -le $2 ] ; then _passed ; else _failed "$1" "$2" ; fi
+}
---
+++ src/script/randori.bash
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+
+# Extended from BashUnit:
+# https://github.com/djui/bashunit
+#
+
+source $(dirname 0)/assert.bash
+
+########################################################################
+# UTILS
+########################################################################
+
+# Random number.
+#
+# $1: Max value
+rnd() {
+    local result=$(( ( RANDOM % $1 )  + 1 ))
+    echo $result
+}
+
+# Skip assert.
+#
+skip() {
+    _skipped
+}
+
+########################################################################
+# OUTCOME
+########################################################################
+
+_failed() {
+    asserts_failed=$((asserts_failed+1))
+
+    local line=${BASH_LINENO[1]}
+    local src="eval sed -ne \"$line p\" ${BASH_SOURCE[2]}"
+    local assert=($($src))
+
+    if [ $verbose -ge 2 ] ; then
+        if [ $lineshow -eq 1 ]; then
+            failed_line="$($src)"
+        else
+            failed_line=
+        fi
+        echo -e "\033[31;1m ✘ \033[0m$assert:$line\033[90;1m${failed_line}\033[0m"
+    fi
+    if [ $verbose -eq 3 ] ; then
+        echo -e "\033[31m   Expected\033[0m: $(sed '2,$ s/^/          /g' <<<$2)"
+        echo -e "\033[31m   Provided\033[0m: $(sed '2,$ s/^/          /g' <<<$1)"
+    fi
+}
+
+_passed() {
+    asserts_passed=$((asserts_passed+1))
+
+    local line=${BASH_LINENO[1]}
+    local src="eval sed -ne \"$line p\" ${BASH_SOURCE[2]}"
+    local assert=($($src))
+    if [ $verbose -ge 2 ] ; then
+        if [ $lineshow -eq 1 ]; then
+            success_line="$($src)"
+        else
+            success_line=
+        fi
+        printf "\033[32;1m ✔ \033[0m$assert:$line\033[90;1m${success_line}%-30s$eol\033[0m"
+    fi
+}
+
+_skipped() {
+    asserts_skipped=$((asserts_skipped+1))
+
+    local line=${BASH_LINENO[1]}
+    local src="eval sed -ne \"$line s/skip //; $line p\" ${BASH_SOURCE[2]}"
+    local assert=($($src))
+    if [ $verbose -ge 2 ] ; then
+        if [ $lineshow -eq 1 ]; then
+            skipped_line="$($src)"
+        else
+            skipped_line=
+        fi
+        printf "\033[33;1m - \033[0m$assert:$line\033[90;1m${skipped_line}%-30s$eol\033[0m"
+    fi
+}
+
+########################################################################
+# RUN
+########################################################################
+verbose=2
+lineshow=0
+eol="\n"
+
+usage() {
+    echo "Usage: <testscript> [options...]"
+    echo
+    echo "Options:"
+    echo "  -v, --verbose   Print expected and provided values"
+    echo "  -s, --summary   Only print summary omitting individual test results"
+    echo "  -q, --quiet     Do not print anything to standard output"
+    echo "  -l, --lineshow  Show failing or skipped line after line number"
+    echo "  -f, --failed    Print only individual failed test results"
+    echo "  -h, --help      Show usage screen"
+}
+
+run() {
+    # Find and declare 'test_" functions
+    local test_pattern="test[a-zA-Z0-9_]\+"
+    local tests=$(declare -F | \
+        sed -ne '/'"$test_pattern"'$/ { s/declare -f // ; p }')
+
+    # No tests found
+    if [ ! "${tests[*]}" ] ; then
+        usage
+        exit 0
+    fi
+
+    # Setup
+    if [ "$(LC_ALL=C type -t setup)" = function ]; then setup; fi
+
+    # Run tests
+    for test in $tests ;
+    do
+        if [ $verbose -ge 1 ] ; then
+            echo -e "\033[37;1m$test\033[0m"
+        fi
+        $test
+    done
+
+    # Teardown
+    if [ "$(LC_ALL=C type -t teardown)" = function ]; then teardown; fi
+
+    # Report
+    if [ $verbose -gt 1 ] ; then
+        printf "\033[K"
+    fi
+    if [ $verbose -ge 1 ] ; then
+        echo "Done. $asserts_passed passed." \
+             "$asserts_failed failed." \
+             "$asserts_skipped skipped."
+    fi
+    exit $asserts_failed
+}
+
+# Arguments
+while [ $# -gt 0 ]; do
+    arg=$1; shift
+    case $arg in
+        "-v"|"--verbose") verbose=3;;
+        "-s"|"--summary") verbose=1;;
+        "-q"|"--quiet")   verbose=0;;
+        "-l"|"--lineshow") lineshow=1;;
+        "-f"|"--failed")   eol="\r";;
+        "-h"|"--help")    usage; exit 0;;
+        *) shift;;
+    esac
+done
+
+run
